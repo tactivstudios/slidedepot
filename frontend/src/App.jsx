4a6d229f37aa16e8cc75fe0938a85297b2ed485d
Jaco-Miras <miras.jaco@gmail.com>
2022-04-07 10:59:49 +0800

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Authentication/Login/Login";
-import Signup from "./components/Authentication/Signup/Signup";
-import Landing from "./components/Users/Navbar/Landing";
+import About from "@/components/About/About";
+import Login from "@/components/Authentication/Login/Login";
+import Signup from "@/components/Authentication/Signup/Signup";
+import GuestGallery from "@/components/Guest/Gallery/GuestGallery";
+import Landing from "@/components/Users/Navbar/Landing";
+import "@/App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/landing" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<GuestGallery />} />
       </Routes>
     </BrowserRouter>
   );
