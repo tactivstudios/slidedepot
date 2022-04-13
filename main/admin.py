e77ff0dcac0b6b71b257f7fceb8a05503848ba94
M4ykel <m.johntd@gmail.com>
2022-04-13 09:30:23 +0800

    fixed admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,10 @@
+# from django.contrib.admin.models import LogEntry
 from django.contrib import admin
-from .models import Article
+from .models import (
+    Article,
+    Presentation,
+    Category
+)
 
 # Register your models here.
 
@@ -9,3 +14,9 @@ from .models import Article
 class ArticleModel(admin.ModelAdmin):
     list_filter = ('title', 'description')
     list_display = ('title', 'description')
+
+
+admin.site.register(Category)
+admin.site.register(Presentation)
+
+# LogEntry.objects.all().delete()
