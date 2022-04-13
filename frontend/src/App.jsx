a90ec7fb4b1f46ff872a0e1a01b2b2748317f834
M4ykel <m.johntd@gmail.com>
2022-04-13 09:27:12 +0800

    fixed models

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Authentication/Login/Login";
-import Signup from "./components/Authentication/Signup/Signup";
-import Landing from "./components/Users/Navbar/Landing";
+import Login from "@/components/Authentication/Login/Login";
+import Signup from "@/components/Authentication/Signup/Signup";
+import Landing from "@/components/Users/Navbar/Landing";
+import UserProfile from "@/pages/Users/UserProfile";
+import UploadPresentation from "@/pages/Users/UploadPresentation";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/landing" element={<Landing />} />
+        <Route path="/signup/" element={<Signup />} />
+        <Route path="/landing/" element={<Landing />} />
+
+        <Route path="/profile/" element={<UserProfile />} />
+        <Route path="/upload/" element={<UploadPresentation />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
   },
+  plugins: [react()],
 });
