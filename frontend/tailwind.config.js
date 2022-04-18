b2b7f2383c2932f4c7501602a0452e1c59a1bbdf
M4ykel <m.johntd@gmail.com>
2022-04-18 17:55:13 +0800

    adding plugins for tw-elements

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
     fontFamily: {
       font: ["Inter", "sans-serif"],
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
