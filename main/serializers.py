aa48a790b0707bf6fe9e46b23d00c43840da5213
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-19 15:51:14 +0800

    remove comments

--- main/serializers.py
+++ main/serializers.py
@@ -1,36 +1,8 @@
-# import email
 from rest_framework import serializers
-# from django.contrib.auth.models import User
-# from rest_framework.authtoken.views import Token
 from .models import (
-    # Article,
     Category,
     Presentation,
 )
-
-
-# class ArticleSerializers(serializers.ModelSerializer):
-#     class Meta:
-#         model = Article
-#         fields = ['id', 'title', 'description']
-
-
-# class UserSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = User
-#         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
-
-#         extra_kwargs = {'password': {
-#             'write_only': True,
-#             'required': True,
-#         }}
-
-#     def create(self, validated_data):
-#         user = User.objects.create_user(**validated_data)
-#         Token.objects.create(user=user)
-#         return user
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
