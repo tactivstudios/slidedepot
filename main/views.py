58c7855dd8e9a6b1e13e4991c9fd389ee578c636
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-19 22:18:57 +0800

    Instance Class

--- main/views.py
+++ main/views.py
@@ -1,17 +1,13 @@
 from cgitb import lookup
 from django import http
 from .serializers import (
-    UserSerializer,
     CategorySerializer,
     PresentationSerializer,
 )
 from .models import (
-    User,
     Category,
     Presentation,
 )
-from rest_framework import viewsets, filters
-from rest_framework.permissions import IsAuthenticated
 
 
 # For Upload Presentation
@@ -23,25 +19,6 @@ from rest_framework import status
 from main import serializers
 
 
-#bag o
-class UserViewSet(viewsets.ModelViewSet):
-    http_method_names = ['get']
-    serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated,)
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['updated']
-
-    def get_queryset(self):
-        if self.request.user.is_superuser:
-            return User.objects.all()
-
-    def get_object(self):
-        lookup_field_value = self.kwargs[self.lookup_field]
-
-        obj = User.objects.get(lookup_field_value)
-        self.check_object_permissions(self.request, obj)
-
-        return obj
 
 class CategoryView(APIView):
     parser_classes = (MultiPartParser, FormParser)
@@ -54,17 +31,16 @@ class CategoryView(APIView):
 
 class PresentationView(APIView):
     parser_classes = (MultiPartParser, FormParser)
+    serializer_class = PresentationSerializer
 
     def get(self, request, *args, **kwargs):
         presentations = Presentation.objects.all()
-        serializer = PresentationSerializer(presentations, many=True)
+        serializer = self.serializer_class(presentations, many=True)
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
-        presentation_serializer = PresentationSerializer(data=request.data)
-        if presentation_serializer.is_valid():
-            presentation_serializer.save()
-            return Response(presentation_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print('ERROR', presentation_serializer.errors)
-            return Response(presentation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
