8beaf3e4fb388c2d82ecdda25b2ff250b08a8f7a
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-19 22:19:42 +0800

    remove username

--- frontend/src/components/Authentication/Signup/Signup.jsx
+++ frontend/src/components/Authentication/Signup/Signup.jsx
@@ -20,8 +20,6 @@ function Signup() {
           </Link>        
         </p>
         <div className="flex flex-col">
-        <h1 className="my-1 font-semibold text-sm">User Name</h1>
-          <input className="form-input" type="text" placeholder="User Name" />
           <h1 className="my-1 font-semibold text-sm">First Name</h1>
           <input className="form-input" type="text" placeholder="First Name" />
           <h1 className="my-1 font-semibold text-sm">Last Name</h1>
