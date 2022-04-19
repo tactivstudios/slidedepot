3b0f7aae0801ff9fdfcb9034f097c127a58bd032
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-19 16:00:43 +0800

    remove comments in views.py

--- main/views.py
+++ main/views.py
@@ -1,11 +1,8 @@
 from .serializers import (
-    # ArticleSerializers,
-    # UserSerializer,
     CategorySerializer,
     PresentationSerializer,
 )
 from .models import (
-    # Article,
     Category,
     Presentation,
 )
@@ -20,22 +17,6 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 
-
-# from django.views.decorators.csrf import csrf_exempt
-
-# Get_post
-# @csrf_exempt
-
-# class ArticleViewSet(viewsets.ModelViewSet):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializers
-#     permission_classes = [IsAuthenticated]
-#     authentication_classes = (TokenAuthentication)
-
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
 
 
 class CategoryView(APIView):
