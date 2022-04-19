4f3fe8994bb3b3c4f6ef1dbeac533694b75dc2a2
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-19 16:09:33 +0800

    Added usUser in Admin

--- main/admin.py
+++ main/admin.py
@@ -1,21 +1,13 @@
 # from django.contrib.admin.models import LogEntry
 from django.contrib import admin
 from .models import (
-    # Article,
+    User,
     Presentation,
     Category
 )
 
-# Register your models here.
 
-
-# admin.site.register(Article)
-# @admin.register(Article)
-# class ArticleModel(admin.ModelAdmin):
-#     list_filter = ('title', 'description')
-#     list_display = ('title', 'description')
-
-
+admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Presentation)
 
