133f18d950fe85865ccbb1223867ffeecb0e6ad1
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-20 21:53:27 +0800

    added user register viewset

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
+    RegisterViewSet,
     CommentView,
-    CategoryView,
-    PresentationView,
 )
 
 router = DefaultRouter()
-
+router.register('users', RegisterViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('upload-presentation/', PresentationView.as_view(),name='upload-presentation'),
-    path('category/', CategoryView.as_view(), name='category'),
     path('comment/', CommentView.as_view(), name='comment')
 ]
