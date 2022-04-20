d36347d7a208257ee69398d1c8edc0d1a4668351
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-20 21:55:16 +0800

    remove category imports & added RegisterViewSet

--- main/views.py
+++ main/views.py
@@ -1,50 +1,34 @@
 from .serializers import (
-    CategorySerializer,
-    PresentationSerializer,
+    UserSerializer,
     CommentSerializer
 )
 from .models import (
-    Category,
-    Presentation,
+    User,
     Comment
 )
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
-
-# For Upload Presentation
+from rest_framework.decorators import APIView
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 
-class CategoryView(APIView):
-    parser_classes = (MultiPartParser, FormParser)
+class RegisterViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def get(self, request, *args, **kwargs):
-        category = Category.objects.all()
-        serializer = CategorySerializer(category, many=True)
-        return Response(serializer.data)
+    # def list(self, request):
+    #     users = User.objects.all()
+    #     serializer = UserSerializer(users, many=True)
+    #     return Response(serializer.data)
 
+    # def create(self, request):
+    #     serializer = UserSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
-class PresentationView(APIView):
-    parser_classes = (MultiPartParser, FormParser)
-
-    def get(self, request, *args, **kwargs):
-        presentations = Presentation.objects.all()
-        serializer = PresentationSerializer(presentations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        presentation_serializer = PresentationSerializer(data=request.data)
-        if presentation_serializer.is_valid():
-            presentation_serializer.save()
-            return Response(presentation_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            print('ERROR', presentation_serializer.errors)
-            return Response(presentation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Comment
 class CommentView(APIView):
