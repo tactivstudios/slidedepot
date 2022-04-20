7e25a8fb0b74e614ccf1f9877a65272ca8bd963b
M4ykel <m.johntd@gmail.com>
2022-04-20 18:21:49 +0800

    adding presentation urls

--- slidedepot/urls.py
+++ slidedepot/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('auth/', obtain_auth_token)
+    path('auth/', obtain_auth_token),
+    path('', include('presentation.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
