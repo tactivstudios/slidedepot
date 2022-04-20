ae9afcbf477f89a65eea30518b8f8578ceb22a95
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-20 13:52:21 +0800

    added User

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,14 @@
 # from django.contrib.admin.models import LogEntry
 from django.contrib import admin
 from .models import (
+    User,
     Presentation,
     Category,
     Comment
 )
 
 # Register your models here.
-
+admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Presentation)
 admin.site.register(Comment)
