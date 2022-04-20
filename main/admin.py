38f7caa39917c8e4d18ac02cbda669f073b7e5f2
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-20 21:50:41 +0800

    delete category and presentation

--- main/admin.py
+++ main/admin.py
@@ -2,15 +2,11 @@
 from django.contrib import admin
 from .models import (
     User,
-    Presentation,
-    Category,
     Comment
 )
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Presentation)
 admin.site.register(Comment)
 
 # LogEntry.objects.all().delete()
