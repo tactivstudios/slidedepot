468542f4cce22de36eac0591c34c5c1976f9bd4c
M4ykel <m.johntd@gmail.com>
2022-04-20 12:21:54 +0800

    fixed grid gap presentation card

--- frontend/src/components/Users/PresentationCard.jsx
+++ frontend/src/components/Users/PresentationCard.jsx
@@ -7,16 +7,14 @@ export default class PresentationCard extends Component {
 
   render() {
     return (
-      <div className="grid grid-cols-4 gap-11 max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <img
           className="w-full"
           src={this.props.item.thumbnail_image}
           alt="Presentation"
         />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">
-            {this.props.item.title}
-          </div>
+          <div className="font-bold text-xl mb-2">{this.props.item.title}</div>
           <p className="text-gray-700 text-base">sample name sample name</p>
         </div>
       </div>
