1351a620015a5107ab2088f651e3d30d4fa4f479
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-20 13:35:30 +0800

    Fixing Conflicts

--- main/models.py
+++ main/models.py
@@ -1,15 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils import timezone
 
 # Create your models here.
+class CustomeUserManager(BaseUserManager):
+    def _create_user(self, email, password, first_name, last_name, **extra_fields):
+        if not email: 
+            raise ValueError("Email must be provided")
+        if not password:
+            raise ValueError("Password is not provided")
 
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            **extra_fields
+        )
 
-class Article(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    def __str__(self):
-        return self.title
+    def create_user(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password,first_name, last_name, **extra_fields)
+
+    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
+class User(AbstractBaseUser, PermissionsMixin):
+        email = models.EmailField(db_index=True, unique=True, max_length=254)
+        first_name = models.CharField(max_length=240)
+        last_name = models.CharField(max_length=255)
+
+        is_staff = models.BooleanField(default=True)
+        is_active = models.BooleanField(default=True)
+        is_superuser = models.BooleanField(default=False)
+
+        objects = CustomeUserManager()
+
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = 'first_name', 'last_name', 'password'
 
 
 class Category(models.Model):
