f587f0e6b217a1940c45e859f84627a8e580f397
M4ykel <m.johntd@gmail.com>
2022-04-21 18:29:34 +0800

    removed route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Login from "@/components/Authentication/Login/Login";
 import Signup from "@/components/Authentication/Signup/Signup";
 import Landing from "@/components/Users/Navbar/Landing";
 import UserProfile from "@/pages/Users/UserProfile";
-import UploadPresentation from "@/pages/Users/UploadPresentation";
 
 function App() {
   return (
@@ -12,9 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup/" element={<Signup />} />
         <Route path="/landing/" element={<Landing />} />
-
         <Route path="/profile/" element={<UserProfile />} />
-        <Route path="/upload/" element={<UploadPresentation />} />
       </Routes>
     </BrowserRouter>
   );
