8521765da893e5f624854ba618f7563ac043a323
Jaco-Miras <miras.jaco@gmail.com>
2022-04-22 15:46:10 +0800

    added about

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,20 @@ import Login from "@/components/Authentication/Login/Login";
 import Signup from "@/components/Authentication/Signup/Signup";
 import Landing from "@/components/Users/Navbar/Landing";
 import UserProfile from "@/pages/Users/UserProfile";
+import GuestGallery from "@/components/Guest/Gallery/GuestGallery";
+import About from "@/components/About/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<GuestGallery />} />
+        <Route path="/about/" element={<About />} />
+        <Route path="/login/" element={<Login />} />
         <Route path="/signup/" element={<Signup />} />
         <Route path="/landing/" element={<Landing />} />
         <Route path="/profile/" element={<UserProfile />} />
+
       </Routes>
     </BrowserRouter>
   );
