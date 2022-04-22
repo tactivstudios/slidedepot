1f3a178f8f07ab020fa3bc0875bb123886cc497f
Jaco-Miras <miras.jaco@gmail.com>
2022-04-22 15:47:14 +0800

    added about

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         <Route path="/signup/" element={<Signup />} />
         <Route path="/landing/" element={<Landing />} />
         <Route path="/profile/" element={<UserProfile />} />
-
       </Routes>
     </BrowserRouter>
   );
