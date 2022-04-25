0a8fbcfc73112e4ced813350dc4eb55195b93594
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-25 21:21:33 +0800

    added auth useradmin

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import (
     User,
     Comment
 )
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    pass
+
 admin.site.register(User)
 admin.site.register(Comment)
