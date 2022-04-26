79c7a2cb83c7c8911dbbd791fdf15979eaa5892e
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-27 01:07:50 +0800

    removing uneccesary imports

--- main/urls.py
+++ main/urls.py
@@ -2,14 +2,12 @@ from email.mime import base
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    AuthViewSet,
     RegisterViewSet,
     CommentView,
 )
 
 router = DefaultRouter()
 router.register('users', RegisterViewSet, basename='users')
-router.register('auth/', AuthViewSet, basename='auth')
 
 urlpatterns = [
     path('api/', include(router.urls)),
