52c294660e83896cb49f5b360a181b24dad1b845
jemarsoliva <jemarsoliva4@gmail.com>
2022-04-26 22:37:46 +0800

    added login serializer

--- main/serializers.py
+++ main/serializers.py
@@ -1,8 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from .models import (
     User,
     Comment
 )
+
+from django.contrib.auth import password_validation
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -29,6 +33,38 @@ class UserSerializer(serializers.ModelSerializer):
             instance.save()
 
         return instance
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(max_length=300, required = True)
+    password = serializers.CharField(required = True, write_only = True)
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    auth_token = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        read_only_fields = ['id', 'is_active', 'is_staff']
+
+    def get_auth_token(self, obj):
+        token = Token.objects.create(user=obj)
+        return token.key
+
+class EmptySerializer(serializers.ModelSerializer):  
+    pass      
+
+class PasswordChangeSerializer(serializers.ModelSerializer):
+    current_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validated_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError('Current password does not match')
+        return value
+    
+    def validated_new_password(self, value):
+        password_validation.validate_password(value)
+        return value
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
