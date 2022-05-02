6d0d6e2d8c4dd2befefee5fb2f8bc4c44e256ae6
jemarsoliva <jemarsoliva4@gmail.com>
2022-05-02 15:15:57 +0800

    remove unneccesary url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
         <Route path="/" element={<GuestGallery />} />
         <Route {...<ProtectedLogin/>} path="/login/" element={<Login />}auth={setAuth.auth} />
         <Route path="/about/" element={<About />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/landing/" element={<Landing />} />
         <Route path="/profile/:id/" element={<UserProfile />} />
