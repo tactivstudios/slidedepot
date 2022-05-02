19eab03a2cf80180d850a06cda764a96224dcd4c
jemarsoliva <jemarsoliva4@gmail.com>
2022-05-02 18:15:33 +0800

    Added ChangerPasswordViewset

--- main/urls.py
+++ main/urls.py
@@ -3,13 +3,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     RegisterViewSet,
+    ChangePasswordViewset,
     CommentView,
 )
 
 router = DefaultRouter()
 router.register('users', RegisterViewSet, basename='users')
+# router.register(r'change-password', ChangePasswordViewset, basename='change-password')
+# router.register('password',ChangePasswordViewset, basename='password')
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('comment/', CommentView.as_view(), name='comment'),
+    path('api/change-password/', ChangePasswordViewset.as_view(), name='change-password'),
 ]
