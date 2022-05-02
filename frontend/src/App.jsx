37e351d8398ca47b9e4b53ad0846eb6e58bf105e
jemarsoliva <jemarsoliva4@gmail.com>
2022-05-02 14:04:02 +0800

    fixed conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "@/components/Authentication/Login/Login";
 import Signup from "@/components/Authentication/Signup/Signup";
@@ -5,18 +6,24 @@ import Landing from "@/components/Users/Navbar/Landing";
 import UserProfile from "@/pages/Users/UserProfile";
 import GuestGallery from "@/components/Guest/Gallery/GuestGallery";
 import About from "@/components/About/About";
+import AuthContext from "@/context/AuthProvider";
+import ProtectedLogin from "@/components/Authentication/ProtectedLogin";
 
 function App() {
+  const setAuth = React.useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GuestGallery />} />
+        <Route {...<ProtectedLogin/>} path="/login/" element={<Login />}auth={setAuth.auth} />
         <Route path="/about/" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/landing/" element={<Landing />} />
         <Route path="/profile/:id/" element={<UserProfile />} />
       </Routes>
+
+
     </BrowserRouter>
   );
 }
