dad31eeb9b1307dab38d6030d7cf018a2c3ce0f2
M4ykel <m.johntd@gmail.com>
2022-05-03 18:44:04 +0800

    added config file for API server

---
+++ frontend/src/APIService/config.jsx
@@ -0,0 +1,9 @@
+// Backend Server
+export const BACKEND_URL = "http://127.0.0.1:8000";
+
+// API Endpoints
+export const API_GET_CATEGORY = `${BACKEND_URL}/category/`;
+export const API_PT_UPLOAD = `${BACKEND_URL}/upload-presentation/`;
+export const API_PROFILE_DETAILS = `${BACKEND_URL}/profile-details/`;
+export const API_PT_DETAILS = `${BACKEND_URL}/pt-details/`;
+export const API_USERS = `${BACKEND_URL}/api/users/`;
