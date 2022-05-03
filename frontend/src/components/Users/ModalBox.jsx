910876fe2b23610b9287dcc5173b1a29a4402545
M4ykel <m.johntd@gmail.com>
2022-05-03 18:40:58 +0800

    ModalBox for modals

---
+++ frontend/src/components/Users/ModalBox.jsx
@@ -0,0 +1,27 @@
+export const div_style_one = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "30vw",
+  height: "90vh",
+  bgcolor: "background.paper",
+  boxShadow: 20,
+  overflowY: "scroll",
+  p: 5,
+  borderRadius: 2,
+};
+
+export const div_style_two = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -0%)",
+  width: "30vw",
+  height: "90vh",
+  bgcolor: "background.paper",
+  boxShadow: 20,
+  overflowY: "scroll",
+  p: 5,
+  borderRadius: 2,
+};
