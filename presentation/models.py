4dc4b23d33be86b1e804ece0cff543961caa7fc4
M4ykel <m.johntd@gmail.com>
2022-05-03 18:30:15 +0800

    id changes, added author for User

--- presentation/models.py
+++ presentation/models.py
@@ -16,7 +16,8 @@ class Category(models.Model):
 
 
 class Presentation(models.Model):
-    id = models.CharField(primary_key=True, max_length=200, unique=True)
+    presentation_id = models.CharField(
+        primary_key=True, max_length=200, unique=True)
     file = models.FileField(upload_to='save_file', null=True, blank=True)
     file_name = models.TextField(null=True, blank=True)
     thumbnail_image = models.ImageField(
@@ -25,6 +26,8 @@ class Presentation(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True)
     date_posted = models.DateField(_("Date"), auto_now_add=True)
+    author = models.ForeignKey(
+        'main.User', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
