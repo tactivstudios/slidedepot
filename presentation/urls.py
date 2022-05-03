574cba8752fde441fcea22549b4fc1507ca948f7
M4ykel <m.johntd@gmail.com>
2022-05-03 18:32:47 +0800

    added url (profile-details)

--- presentation/urls.py
+++ presentation/urls.py
@@ -3,11 +3,13 @@ from .views import (
     CategoryView,
     PresentationPost,
     PresentationDetail,
+    ProfileDetail,
 )
 
 
 urlpatterns = [
     path('category/', CategoryView.as_view(), name='category'),
     path('upload-presentation/', PresentationPost.as_view(), name='upload-presentation'),
-    path('pt-details/<str:id>/', PresentationDetail.as_view(), name='presentation-detail'),
+    path('pt-details/<str:presentation_id>/', PresentationDetail.as_view(), name='presentation-detail'),
+    path('profile-details/<int:author>/', ProfileDetail.as_view(), name='profile-detail'),
 ]
