0a8fe71366f13ac1cfb0b56122cb438543270436
jemarsoliva <jemarsoliva4@gmail.com>
2022-05-04 23:25:27 +0800

    added login

--- slidedepot/urls.py
+++ slidedepot/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
 from django.conf.urls.static import static
+
+from main.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('auth/', obtain_auth_token),
+    path('auth/', LoginView.as_view()),
     path('', include('presentation.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
