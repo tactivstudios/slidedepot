e7fb501cab21c9cb6477d0e1d2797943a7db0a5e
M4ykel <m.johntd@gmail.com>
2022-05-12 20:09:21 +0800

    added urls changes

--- presentation/urls.py
+++ presentation/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from .views import (
     CategoryView,
     PresentationPost,
+    PresentationGet,
     PresentationDetail,
-    ProfileDetail,
 )
 
 
 urlpatterns = [
     path('category/', CategoryView.as_view(), name='category'),
-    path('upload-presentation/', PresentationPost.as_view(), name='upload-presentation'),
-    path('pt-details/<str:presentation_id>/', PresentationDetail.as_view(), name='presentation-detail'),
-    path('profile-details/<int:author>/', ProfileDetail.as_view(), name='profile-detail'),
+    path('upload-presentation/', PresentationPost.as_view(),
+         name='upload-presentation'),
+    path('get-presentation/', PresentationGet.as_view(), name='get-presentation'),
+    path('pt-details/<str:presentation_id>/',
+         PresentationDetail.as_view(), name='presentation-detail'),
 ]
