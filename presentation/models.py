fe38d0925ca13a062ff219960df2da3fc33b57d5
M4ykel <m.johntd@gmail.com>
2022-05-12 20:08:55 +0800

    arranged models

--- presentation/models.py
+++ presentation/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
@@ -25,13 +25,12 @@ class Category(models.Model):
 class Presentation(models.Model):
     presentation_id = models.CharField(
         default=generate_id, primary_key=True, max_length=200, unique=True)
-    file = models.FileField(upload_to='save_file', null=True, blank=True)
-    file_name = models.TextField(null=True, blank=True)
+    file = models.FileField(upload_to='save_file', null=True)
+    file_name = models.TextField(null=True)
     thumbnail_image = models.ImageField(
-        upload_to='thumbnail_images', null=True, blank=True)
-    title = models.TextField(null=True, blank=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True)
+        upload_to='thumbnail_images', null=True)
+    title = models.TextField(max_length=200, null=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
     date_posted = models.DateField(_("Date"), auto_now_add=True)
     author = models.ForeignKey(
         'main.User', null=True, on_delete=models.CASCADE)
