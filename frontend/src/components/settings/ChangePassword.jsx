2f482fb68417a254cb6cd7407815b878bc9e7709
jemarsoliva <jemarsoliva4@gmail.com>
2022-05-12 22:18:17 +0800

    last modified

--- frontend/src/components/settings/ChangePassword.jsx
+++ frontend/src/components/settings/ChangePassword.jsx
@@ -1,34 +1,47 @@
 import React from "react";
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
-
 
 import axios from "@/APIService/axios";
 const PASSWORD_URL = '/api/change-password/';
 
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 export default function ChangePassword() {
     const navigate = useNavigate();
-    const [new_password, confirm_password] = useState();
-    const [matchPwd, setMatchPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-
+    const errRef = useRef();
+    
     const [user, setUser] = useState({
     old_password: "",
     new_password: "",
     confirm_password: "",
     });
+    const validPassword = useState(false);
+    const passwordFocus = useState(false);
 
+    const [errMsg, setErrMsg] = useState('');
+    
+    useEffect(() => { 
+      setErrMsg('');
+    }, [user.new_password, user.confirm_password])
+
+    useEffect(() => {
+      const result = PWD_REGEX.test(user.new_password);
+      console.log(result);
+      console.log(user.new_password);
+    }, [user.new_password])
+    
     const updatePass = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
     };
     const submitHandler = async (e) => {
-
+    
     e.preventDefault();
-    if(new_password !== confirm_password){
-        setErrMsg("password_mismatch");
+    if(user.new_password != user.confirm_password){
+        setErrMsg("Password Mismatch!");
         return;
-    }
+    }else{
+
     try{
         const response = await axios.put(PASSWORD_URL, 
               JSON.stringify({old_password: user.old_password, new_password: user.new_password}), 
@@ -40,13 +53,16 @@ export default function ChangePassword() {
             navigate("/landing/")
       } 
         catch(error){
-            console.log(error)
-            alert(error)
+          console.log(error)
+          alert(error) 
           }
+      }
     };
 
   return (
+    <div>
       <div>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
         <form onSubmit={submitHandler}>  
             <label className="my-1 font-semibold text-sm">Old Password</label>
             <input 
@@ -64,9 +80,21 @@ export default function ChangePassword() {
                 name="new_password"
                 type="password"
                 onChange={updatePass}
+                aria-invalid={validPassword ? "false" : "true"}
                 value={user.new_password}
                 required
             />
+            <p id="pwdnote" className={passwordFocus && !validPassword ? "instructions" : "offscreen"}>
+            <span className='text-sm'>
+              
+               4 to 8 character. <br />
+              Must include uppercase and lowecase letters, a number and a special character. <br />
+              Allowed special characters: <span aria-label='exclamation'>!</span>
+              <span aria-label='at symbol'>@</span><span aria-label='hashtag'>#</span>
+              <span aria-label='dollar sign'>$</span>
+              
+            </span>
+            </p>            
             <label className="my-1 font-semibold text-sm">Confirm Password</label>
             <input 
                 id="confirm_password"
@@ -74,11 +102,13 @@ export default function ChangePassword() {
                 type="password"
                 onChange={updatePass}
                 value={user.confirm_password}
-                error = {setErrMsg}
                 required
             />
-            <button className="btn-default rounded-md px-4 py-1 text-white" >Update</button>
+            <button className="btn-default rounded-md px-4 py-1 text-white">
+              Update
+            </button>
         </form>
+        </div>
     </div>
   );
 };
