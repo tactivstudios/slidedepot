77192a65d1a341bbb49731ec13a0e1f07b5d2bb2
jemarsoliva <jemarsoliva4@gmail.com>
2022-05-20 20:08:31 +0800

    added user serializer

--- main/serializers.py
+++ main/serializers.py
@@ -18,17 +18,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
-    # def update(self, instance, validated_data):
-
-    #     password = validated_data.pop('password', None)
-    #     instance = super(RegisterSerializer, self).update(instance, validated_data)
-
-    #     if password:
-    #         instance.set_password(password)
-    #         instance.save()
-
-    #     return instance
-
+class UserSerializer(serializers.ModelSerializer):
+     class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
 class ChangePasswordSerializer(serializers.Serializer):
     model = User
     """
